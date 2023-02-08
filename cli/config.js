eb79b670afe0cfa6399106547ac696a5c1c9718f
0x10 <hex.ten@protonmail.com>
2023-02-08 14:20:35 +0700

    Check for ledger

--- cli/config.js
+++ cli/config.js
@@ -1,7 +1,7 @@
 export const config = {
   rpc: process.env.COMP_RPC,
-  useLedger: process.env.COMP_USE_LEDGER === 1 ? true : false,
+  useLedger: process.env.COMP_USE_LEDGER == 1 ? true : false,
   ledgerWalletPath: process.env.COMP_LEDGER_PATH,
   adminWallet: process.env.COMP_ADMIN,
-  adminKey: (process.env.COMP_USE_LEDGER !== 1) ? process.env.COMP_ADMIN_KEY : null,
+  adminKey: (process.env.COMP_USE_LEDGER != 1) ? process.env.COMP_ADMIN_KEY : null,
 }
